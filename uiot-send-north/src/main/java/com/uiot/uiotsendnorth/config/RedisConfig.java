898ca981cf2ab533c4d1c3fd456ad74501f770b2
huangkanghui <1263118643@qq.com>
2023-07-13 01:21:54 +0800

    提交第五版

--- uiot-send-north/src/main/java/com/uiot/uiotsendnorth/config/RedisConfig.java
+++ uiot-send-north/src/main/java/com/uiot/uiotsendnorth/config/RedisConfig.java
@@ -1,79 +1,79 @@
-package com.uiot.uiotsendnorth.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
-
-@Configuration
-//@EnableAutoConfiguration
-public class RedisConfig {
-
-    
-    @Value("${spring.redis.password}")
-    private String password;
-    @Value("${spring.redis.host}")
-    private String host;
-    @Value("${spring.redis.port}")
-    private String port;
-
-    @Value("${spring.redis.jedis.pool.max-total}")
-    private int maxTotal;
-    
-    @Value("${spring.redis.jedis.pool.max-wait}")
-    private long maxWait;
-    
-    @Value("${spring.redis.jedis.pool.max-idle}")
-    private int maxIdle;
-    
-    @Value("${spring.redis.jedis.pool.min-idle}")
-    private int minIdle;
-    
-    @Value("${spring.redis.jedis.pool.testWhileIdle}")
-    private boolean testWhileIdle;
-    
-    @Value("${spring.redis.jedis.pool.timeBetweenEvictionRunsMillis}")
-    private long timeBetweenEvictionRunsMillis;
-    
-
-
-
-    @Bean
-    public JedisPoolConfig getRedisConfig(){
-        JedisPoolConfig config = new JedisPoolConfig();
-        config.setMaxTotal(maxTotal);
-        config.setMaxWaitMillis(maxWait);
-        config.setMaxIdle(maxIdle);
-        config.setMinIdle(minIdle);
-        config.setTestWhileIdle(testWhileIdle);
-        config.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        return config;
-    }
-
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisPoolConfig);
-        jedisConnectionFactory.setHostName(host);
-        jedisConnectionFactory.setPort(Integer.parseInt(port));
-        jedisConnectionFactory.setPassword(password);
-        return jedisConnectionFactory;
-    }
-    
-	@Bean
-	public RedisTemplate<Object, Object> redisTemplate(JedisConnectionFactory redisConnectionFactory) {
-//		// StringRedisTemplate的构造方法中默认设置了stringSerializer
-		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        RedisSerializer stringSerializer = new StringRedisSerializer();
-        redisTemplate.setKeySerializer(stringSerializer);
-        redisTemplate.setValueSerializer(stringSerializer);
-        redisTemplate.setHashKeySerializer(stringSerializer);
-        redisTemplate.setHashValueSerializer(stringSerializer);
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-		return redisTemplate;
-	}
-    
-}
+//package com.uiot.uiotsendnorth.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.RedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//import redis.clients.jedis.JedisPoolConfig;
+//
+//@Configuration
+////@EnableAutoConfiguration
+//public class RedisConfig {
+//
+//
+//    @Value("${spring.redis.password}")
+//    private String password;
+//    @Value("${spring.redis.host}")
+//    private String host;
+//    @Value("${spring.redis.port}")
+//    private String port;
+//
+//    @Value("${spring.redis.jedis.pool.max-total}")
+//    private int maxTotal;
+//
+//    @Value("${spring.redis.jedis.pool.max-wait}")
+//    private long maxWait;
+//
+//    @Value("${spring.redis.jedis.pool.max-idle}")
+//    private int maxIdle;
+//
+//    @Value("${spring.redis.jedis.pool.min-idle}")
+//    private int minIdle;
+//
+//    @Value("${spring.redis.jedis.pool.testWhileIdle}")
+//    private boolean testWhileIdle;
+//
+//    @Value("${spring.redis.jedis.pool.timeBetweenEvictionRunsMillis}")
+//    private long timeBetweenEvictionRunsMillis;
+//
+//
+//
+//
+//    @Bean
+//    public JedisPoolConfig getRedisConfig(){
+//        JedisPoolConfig config = new JedisPoolConfig();
+//        config.setMaxTotal(maxTotal);
+//        config.setMaxWaitMillis(maxWait);
+//        config.setMaxIdle(maxIdle);
+//        config.setMinIdle(minIdle);
+//        config.setTestWhileIdle(testWhileIdle);
+//        config.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+//        return config;
+//    }
+//
+//    @Bean
+//    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
+//        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisPoolConfig);
+//        jedisConnectionFactory.setHostName(host);
+//        jedisConnectionFactory.setPort(Integer.parseInt(port));
+//        jedisConnectionFactory.setPassword(password);
+//        return jedisConnectionFactory;
+//    }
+//
+//	@Bean
+//	public RedisTemplate<Object, Object> redisTemplate(JedisConnectionFactory redisConnectionFactory) {
+////		// StringRedisTemplate的构造方法中默认设置了stringSerializer
+//		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+//        RedisSerializer stringSerializer = new StringRedisSerializer();
+//        redisTemplate.setKeySerializer(stringSerializer);
+//        redisTemplate.setValueSerializer(stringSerializer);
+//        redisTemplate.setHashKeySerializer(stringSerializer);
+//        redisTemplate.setHashValueSerializer(stringSerializer);
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//		return redisTemplate;
+//	}
+//
+//}
